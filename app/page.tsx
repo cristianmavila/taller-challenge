e013917f1d9d5de7fc4b4a6dfdf7e8cdb209d248
Cristian Ávila <cristianmachadoavila@gmail.com>
2024-10-29 13:32:00 -0300

    final solution

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,35 @@
+const getData = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:3000/api/payments");
+    const { data } = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+interface TransactionProps {
+  id: number;
+  date: string;
+  description: string;
+  amount: string | number;
+}
+
 export default async function Home() {
-  const response = await fetch("http://127.0.0.1:3000/api/payments");
-  console.log(response);
+  const data = await getData();
   return (
     <div className="mx-auto my-8 max-w-2xl">
       <h1>Taller challenge</h1>
       <ul className="my-4">
-        <li className="border-white border-t-2 grid grid-cols-4 py-4">
-          <div>12</div>
-          <div>12/12/2024</div>
-          <div>Description</div>
-          <div>Amount</div>
-        </li>
-        <li className="border-white border-t-2 grid grid-cols-4 py-4">
-          <div>12</div>
-          <div>12/12/2024</div>
-          <div>Description</div>
-          <div>Amount</div>
-        </li>
-        <li className="border-white border-t-2 grid grid-cols-4 py-4">
-          <div>12</div>
-          <div>12/12/2024</div>
-          <div>Description</div>
-          <div>Amount</div>
-        </li>
+        {data.length > 0 &&
+          data?.map(({ id, date, description, amount }: TransactionProps) => (
+            <li className="border-white border-t-2 grid grid-cols-4 py-4" key={id}>
+              <div>{id}</div>
+              <div>{date}</div>
+              <div>{description}</div>
+              <div>{amount}</div>
+            </li>
+          ))}
       </ul>
     </div>
   );
